dc19654750bcbebf5508eca0273d147635353e8e
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-03-20 12:33:53 +0500

    правим по замечаниям

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,13 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { faker } from '@faker-js/faker';
-import { Types } from 'mongoose';
 import BadRequestError from '../errors/bad-request-error';
 import DefaultError from '../errors/default-error';
 import Product, { IProduct } from '../models/product';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const products = await Product.find({});
     const {
       _payment, _email, _phone, total, _address, items,
     } = req.body;
@@ -17,24 +15,25 @@ export default async (req: Request, res: Response, next: NextFunction) => {
       return next(new BadRequestError('Нет товаров в заказе'));
     }
 
-    // проверка, что все товары есть
-    const productId = new Set(products.map((p) => p._id.toString()));
-    const missingItems = items.filter((item:Types.ObjectId) => !productId.has(item.toString())).join(', ');
-    if (missingItems !== '') {
-      return next(new BadRequestError(`Товары с id: ${missingItems} не найдены`));
+    // запрос полных продуктов по id
+    const foundProducts = await Product.find({ _id: { $in: items } });
+    const foundIds = foundProducts.map((product) => product._id.toString());
+
+    // проверка, что все  продукты есть в магазине
+    if (foundProducts.length !== foundIds.length) {
+      const missingIds = items.filter((id:string) => !foundIds.includes(id)).join(', ');
+      return next(new BadRequestError(`Товары с id: ${missingIds} не найдены`));
     }
 
-    const fullItems = products.filter((product) => items.includes(product.id));
     // проверка, что все товары продаются
-
-    const unavailableItems = fullItems.filter((item: IProduct) => item.price === null);
+    const unavailableItems = foundProducts.filter((item: IProduct) => item.price === null);
     if (unavailableItems.length > 0) {
       const unavailableItemIds = unavailableItems.map((item) => item._id).join(', ');
       return next(new BadRequestError(`Товары с id: ${unavailableItemIds} не продаются`));
     }
 
     // проверка, сумма заказа совпадает с суммой цен товаров
-    const totalPrice = fullItems.reduce((sum: number, item) => sum + (item.price ?? 0), 0);
+    const totalPrice = foundProducts.reduce((sum: number, item) => sum + (item.price ?? 0), 0);
     if (totalPrice !== total) return next(new BadRequestError('Неверная сумма заказа'));
 
     const uuid = faker.string.uuid();
